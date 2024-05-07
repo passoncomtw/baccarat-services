9f79efc77db03ec3b0fe9d60ed80aeaede4bb012
Pineapple Bun <35952622+asdfg44l@users.noreply.github.com>
2024-05-07 15:52:22 +0800

    feat: 🎸 初始化預設資料
    ✅ Closes: #23

--- pkg/baccarat-api/models/Game.go
+++ pkg/baccarat-api/models/Game.go
@@ -6,9 +6,8 @@ import (
 
 type Game struct {
 	gorm.Model
-	Id           uint        `gorm:"primaryKey;comment: '主鍵'"`
-	RedCardIndex int         `gorm:"default:0;comment: '紅卡位置'"`
-	GameOrders   []GameOrder `gorm:"foreignKey:GameRoundId"`
+	RedCardIndex int       `gorm:"default:0;comment: '紅卡位置'"`
+	GameRound    GameRound `gorm:"foreignKey:GameId"`
 }
 
 type CreateGameRequestDto struct {
@@ -18,6 +17,31 @@ type CreateGameRequestDto struct {
 func CreateGame(values CreateGameRequestDto) Game {
 	var game Game
 	game.RedCardIndex = values.redCardIndex
+
+	// rounds
+	gameRoundRequestDto := CreateGameRoundRequestDto{}
+	round := CreateGameRound(&gameRoundRequestDto)
+
+	// orders
+	// orders := []GameOrder{{UserId: 1, GameRoundId: 1}}
+	// game.GameOrders = orders
+
+	game.GameRound = round
+
 	DBConnection.Create(&game)
+	return game
+}
+
+func GetGameById(id uint) *Game {
+	var game *Game
+	DBConnection.Where("id = ?", id).Find(&game)
+
+	return game
+}
+
+func GetGameFirst() *Game {
+	var game *Game
+	DBConnection.First(&game)
+
 	return game
 }
--- pkg/baccarat-api/models/GameOrder.go
+++ pkg/baccarat-api/models/GameOrder.go
@@ -3,40 +3,57 @@ package models
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type GameOrder struct {
 	gorm.Model
-	Id           string     `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	UserId       uint       `gorm:"comment: 'refrence: user.id'"`
-	GameRoundId  uint       `gorm:"comment: 'refrence: game_round.id'"`
+	Id           uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	UserId       uint      `gorm:"foreignKey:ID"`
+	GameId       uint      `gorm:"foreignKey:ID"`
+	GameRoundId  uint
 	BetAmount    int        `gorm:"comment: '下注額度'"`
 	BetType      int        `gorm:"comment: '下注類別: 0: 莊閒和, 1: 點數'"`
 	WinAmount    int        `gorm:"comment: '派彩結果(贏或是輸)'"`
 	Odds         int        `gorm:"comment: '派彩賠率'"`
 	ConcludeTime *time.Time `gorm:"comment: '注單結算時間'"`
-	// GameRound    GameRound  `gorm:"foreignKey:Id"`
-	// User User `gorm:"foreignKey:Id"`
+	// GameRound    GameRound  `gorm:"foreignKey:GameRoundId"`
+	Game Game `gorm:"foreignKey:GameId"`
+	User User `gorm:"foreignKey:UserId"`
 }
 
 type CreateGameOrderRequestDto struct {
-	gameRoundId  uint
-	userId       uint
+	gameRoundId uint
+	// userId       uint
+	// gameId       uint
 	betAmount    int
 	betType      int
 	winAmount    int
 	odds         int
 	concludeTime *time.Time
+	User         User
+	Game         Game
 }
 
-func createGameOrder(values *CreateGameOrderRequestDto) GameOrder {
+func GetGameOrdersByGameRoundId(id uint) *[]GameOrder {
+	var gameOrders *[]GameOrder
+	DBConnection.Find(&gameOrders, "game_round_id = ?", id)
+
+	return gameOrders
+}
+
+func CreateGameOrder(values *CreateGameOrderRequestDto) GameOrder {
 	var gameOrder GameOrder
 	gameOrder.BetAmount = values.betAmount
 	gameOrder.BetType = values.betType
 	gameOrder.WinAmount = values.winAmount
 	gameOrder.Odds = values.odds
 	gameOrder.ConcludeTime = values.concludeTime
-	DBConnection.Create(&gameOrder)
+
+	gameOrder.GameRoundId = values.gameRoundId
+	gameOrder.User = values.User
+	gameOrder.Game = values.Game
+	// DBConnection.Create(&gameOrder)
 	return gameOrder
 }
